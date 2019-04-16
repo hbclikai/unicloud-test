44c9389bae781b9a1c575303f9af498b17d0c3c5
likai <pwd4gitee>
2019-04-16 11:34:41 +0800

    add mysql test

--- src/main/java/com/microcyber/offical/MysqlTest.java
+++ src/main/java/com/microcyber/offical/MysqlTest.java
@@ -9,39 +9,43 @@ public class MysqlTest {
 
         Scanner scanner = new Scanner(System.in);
         //1.dbName
-        System.out.println("enter dbName:");
+        System.out.println("enter ip address of mysql:");
+        String dbIp = scanner.nextLine();
+
+        //2.dbName
+        System.out.println("enter dbName:(for example db10)");
         String dbName = scanner.nextLine();
 
-        //2.username
+        //3.username
         System.out.println("enter username: ");
         String username = scanner.nextLine();
 
-        //3.password
+        //4.password
         System.out.println("enter password: ");
         String password = scanner.nextLine();
 
         Connection con;
         String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://192.168.2.186:3306/"+dbName;
+        String url = "jdbc:mysql://" + dbIp + ":3306/" + dbName;
 
         try {
             //加载驱动程序
             Class.forName(driver);
-            con = DriverManager.getConnection(url,username,password);
-            if(!con.isClosed()){
+            con = DriverManager.getConnection(url, username, password);
+            if (!con.isClosed()) {
                 System.out.println("Succeeded connecting to the Database!");
             }
 
             Statement statement = con.createStatement();
             String sql = "select * from mc_device";
             ResultSet rs = statement.executeQuery(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 long device_id = rs.getLong("device_id");
-                System.out.println("device_id:"+device_id);
+                System.out.println("device_id:" + device_id);
             }
             rs.close();
             con.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
